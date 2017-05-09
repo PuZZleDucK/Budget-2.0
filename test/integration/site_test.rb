8c6d5bae2c7a9ea2037cc5c677a6786723ae7cfc
PuZZleDucK <puzzleduck@gmail.com>
2017-05-09 16:48:07 +1000

    signup errors and tests

--- test/integration/site_test.rb
+++ test/integration/site_test.rb
@@ -9,4 +9,41 @@ class SiteTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", signup_path, count: 2
   end
 
+  test "invalid password" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { first_name: "a",
+                                         email: "invalid@goo.com",
+                                         trust: 0,
+                                         password: "123",
+                                         password_confirmation: "123" } }
+    end
+    assert_template 'users/new'
+    assert_template 'shared/_error_messages'
+  end
+
+  test "invalid email" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { first_name: "a",
+                                         email: "",
+                                         trust: 0,
+                                         password: "123456",
+                                         password_confirmation: "123456" } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { first_name: "a",
+                                         email: "valid@goo.com",
+                                         trust: 0,
+                                         password: "123456",
+                                         password_confirmation: "123456" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
