886d845163f312b713516da48db8ca4d29f4fbc0
PuZZleDucK <puzzleduck@gmail.com>
2017-05-09 14:08:33 +1000

    add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   root 'static_pages#home'
   get 'static_pages/home'
+  get "/signup", to: "users#new"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/integration/site_test.rb
+++ test/integration/site_test.rb
@@ -6,6 +6,7 @@ class SiteTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", signup_path, count: 2
   end
 
 end
