dc74d17799a91d036274769b6a2f93541986609c
PuZZleDucK <puzzleduck@gmail.com>
2017-05-10 00:10:48 +1000

    basic login

--- test/integration/site_test.rb
+++ test/integration/site_test.rb
@@ -46,4 +46,15 @@ class SiteTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
   end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
 end
