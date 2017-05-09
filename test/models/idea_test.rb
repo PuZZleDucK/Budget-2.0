8bfa2d23074d0478add3ba338b816f4a03693f47
PuZZleDucK <puzzleduck@gmail.com>
2017-05-10 08:08:34 +1000

    basic idea model

---
+++ test/models/idea_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class IdeaTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:normal)
+    @idea = @user.ideas.build(content: "content")
+  end
+
+  test "valid" do
+    assert @idea.valid?
+  end
+
+  test "invalid no text" do
+    @idea.content = ""
+    assert_not @idea.valid?
+  end
+
+  test "invalid no user" do
+    @idea.user_id = nil
+    assert_not @idea.valid?
+  end
+
+  test "sorting" do
+    assert_equal ideas(:most_recent), Idea.first
+  end
+
+  test "associated ideas destroyed" do
+    @user.save
+    @user.ideas.create!(content: "temp")
+    assert_difference 'Idea.count', -1 do
+      @user.destroy
+    end
+  end
+
+end
