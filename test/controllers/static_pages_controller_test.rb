dd47edd91f9065166e3cb79e5d8723da02035ffc
PuZZleDucK <puzzleduck@gmail.com>
2017-05-09 13:53:28 +1000

    improve tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "h1", "Welcome to Budget 2.0"
+    assert_select "small", "Hacking the Victorian budget"
   end
 
+  test "root is home" do
+    get root_path
+    assert_response :success
+    assert_select "h1", "Welcome to Budget 2.0"
+    assert_select "small", "Hacking the Victorian budget"
+  end
 end
