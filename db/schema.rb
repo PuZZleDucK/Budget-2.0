cfe580739aded899770d4976b2878fe05ff287f2
PuZZleDucK <puzzleduck@gmail.com>
2017-05-16 06:36:16 +1000

    add initial seed data and start user supports page

--- db/schema.rb
+++ db/schema.rb
@@ -21,17 +21,6 @@ ActiveRecord::Schema.define(version: 20170513024655) do
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.integer  "trust"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
   create_table "supports", force: :cascade do |t|
     t.integer  "idea_id"
     t.integer  "user_id"
@@ -42,6 +31,17 @@ ActiveRecord::Schema.define(version: 20170513024655) do
     t.index ["idea_id", "user_id"], name: "index_supports_on_idea_and_user", unique: true
     t.index ["idea_id"], name: "index_supports_on_idea"
     t.index ["user_id"], name: "index_supports_on_user"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "trust"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# users = User.create([
+#   {first_name: "Ben", last_name: "Minerds", email: "ben@gmail.com", password: "123456"},
+#   {first_name: "Bill", last_name: "Minerds", email: "bill@gmail.com", password: "123456"}
+#   ])
+User.destroy_all
+users = User.create!([
+  {first_name: "Bill", last_name: "Minerds", email: "govhack@gmail.com", password: "123456"},
+  {first_name: "Ben", last_name: "Minerds", email: "hacker@gmail.com", password: "123456"}
+])
+bill = users[0]
+ben = users[1]
+Idea.destroy_all
+ideas = ben.ideas.create!([
+  {content: "bens idea"}
+  ])
+first_idea = ideas[0]
+Support.destroy_all
+bill_supports_first = bill.supports.create!(idea: first_idea)
