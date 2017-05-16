a115a1f7d79315cd19016160600477d1132cac43
PuZZleDucK <puzzleduck@gmail.com>
2017-05-17 02:32:52 +1000

    update user page layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,10 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @ideas = @user.ideas.paginate(page: params[:page])
+    @ideas = @user.ideas
+    if logged_in?
+      @idea = current_user.ideas.build
+    end
   end
 
   # GET /users/new
